a3eb5d52405ed13fbdbfe7796f1921a49c13bf71
Joey <39647@ma-web.nl>
2025-06-23 10:04:12 +0200

    les 4 klaar

---
+++ M4-PROG/Assets/Scenes/les4/playermovment2.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    public float speed = 5f;          
+    public GameObject coinPrefab;       
+
+    void Start()
+    {
+        float randVal = Random.Range(-10, 10);
+        Instantiate(coinPrefab, new Vector3(randVal, 0, 0), Quaternion.identity); 
+    }
+
+    void Update()
+    {
+       
+        float input = Input.GetAxis("Horizontal");
+        
+        transform.Translate(Vector3.right * speed * input * Time.deltaTime);
+    }
+}
